20547839aac6b9c7f6f294ba0e6c0cf1d8dbd075
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-10-28 13:58:15 -0400

    pojo

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,68 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    private String studentId;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String major;
+
+    public Student() {
+        this.studentId = "";
+        this.firstName = "";
+        this.lastName = "";
+        this.age = 0;
+        this.major = "";
+    }
+
+    public Student(String studentId, String firstName, String lastName, int age, String major) {
+        this.studentId = studentId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.major = major;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    @Override
+    public String toString() {
+        return "Student ID: " + studentId + ", Name: " + firstName + " " + lastName + ", Age: " + age + ", Major: " + major;
+    }
 }
--- src/StudentEnrollment.java
+++ src/StudentEnrollment.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class StudentEnrollment {
+public class StudentEnrollment {
+    public static void main(String[] args) {
+        Student student1 = new Student("12345", "John", "Doe", 20, "Computer Science");
+        System.out.println(student1);
+
+        Student student2 = new Student("23456", "Jane", "Smith", 21, "Mathematics");
+        System.out.println(student2);
+
+        Student student3 = new Student("34567", "Bob", "Johnson", 19, "Physics");
+        System.out.println(student3);
+
+        student1.setMajor("Data Science");
+        System.out.println("Updated: " + student1);
+
+
+    }
 }
